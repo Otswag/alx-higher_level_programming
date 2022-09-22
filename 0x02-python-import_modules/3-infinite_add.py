ac704fd58b113402cdd027faf300fd4976efc4b4
Otswag <106761383+Otswag@users.noreply.github.com>
2022-09-22 22:22:31 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     import sys
     result = sum(int(sys.argv[x]) for x in range(1, len(sys.argv)))
-    print("{}".format(resul))
+    print("{}".format(result))
