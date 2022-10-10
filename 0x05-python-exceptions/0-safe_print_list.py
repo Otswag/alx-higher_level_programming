1e249e4431ee58f1a11c4c48b34aa8907a05cc44
Otswag <106761383+Otswag@users.noreply.github.com>
2022-10-10 21:24:20 +0100

    Print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    if (x > 0):
+    enum = 0
+    if(my_list):
         for i in range(x):
             try:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-            except:
-                break
-    print('')
-    return (count)
+                print("{}".format(my_list[i]), end='')
+                enum += 1
+            except IndexError:
+                print()
+                return(printed)
+    print()
+    return(enum)
