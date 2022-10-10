adf1a3b7f000020881434f8c7d74c6bd1a68e9e7
Otswag <106761383+Otswag@users.noreply.github.com>
2022-10-10 21:30:49 +0100

    Ready

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
                 enum += 1
             except IndexError:
                 print()
-                return(printed)
+                return(enum)
     print()
     return(enum)
