d15e337bfe1dfd4fb7b2eb0eefac59342f40a543
Otswag <106761383+Otswag@users.noreply.github.com>
2022-10-25 16:17:43 +0100

    Write file

--- 1-write_file.py
+++ 1-write_file.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-"""Module 1-number_of_lines
-Counts number of ;lines in a file
-"""
+"""writes to a file"""
 
 
-def number_of_lines(filename=""):
-    """Counts lines in filename
-
-    Args:
-        - filename: name of the file
-
-    Returns:
-        - number of lines
-    """
-
-    count = 0
-
-    with open(filename) as f:
-        text = f.readlines()
-        for line in text:
-            count += 1
-
-    return count
+def write_file(filename="", text=""):
+    """writes to file"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return(f.write(text))
