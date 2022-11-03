38ef84ca3e47513134a0d6119cb94355cb39d23d
Otswag <emmanuel.amadi68@gmail.com>
2022-11-03 15:53:36 -0700

    Updated and executable

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
 """Defines a rectangle class."""
 from models.base import Base
 
@@ -8,6 +10,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
+
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
@@ -96,6 +99,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """Update the Rectangle.
+
         Args:
             *args (ints): New attribute values.
                 - 1st argument represents id attribute
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-This module contains the "Square" class
-"""
+# square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
@@ -10,6 +10,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new Square.
             x (int): The x coordinate of the new Square.
@@ -30,6 +31,7 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update the Square.
+
         Args:
             *args (ints): New attribute values.
                 - 1st argument represents id attribute
